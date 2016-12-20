3f55203a5953d82668da582555a5b59dd854a667
Mu <zkfire2345@gmail.com>
2016-12-20 15:38:17 -0500

    fix(merge conflict):

--- client/src/components/post_index.js
+++ client/src/components/post_index.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectEvent } from '../actions/index'
 
-export default () => {
-  return <div>this is the main view</div>
+class PostIndex extends Component {
+  componentWillMount() {
+    console.log(this.props.events);
+    // console.log('this wpld be a a great time to call an actrio creator')
+  }
+
+  render() {
+    return(
+      <div> this.componentWillMount()</div>
+    )
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    events: state.events
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostIndex);
