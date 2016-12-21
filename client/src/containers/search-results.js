f2f0c5462bb8319e340d0423c39c993c79814be1
Timothy Yin <tyin92@gmail.com>
2016-12-20 20:24:11 -0500

    Commit for rebase

--- client/src/containers/search-results.js
+++ client/src/containers/search-results.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+// import { selectEvent } from '../actions/index';
 
 class SearchResults extends Component {
   renderList() {
@@ -46,4 +47,4 @@ function mapStateToProps(state) {
 // Promote BookList from a component to a container - it needs to know
 // about this new dispatch method, selectBook. Make it available
 // as a prop.
-export default connect(mapStateToProps)(SearchResults);
+export default connect(mapStateToProps, null)(SearchResults);
