8f78ee94ab1e95eb758b1d6d6bd6b5361f9e3414
Timothy Yin <tyin92@gmail.com>
2016-12-21 14:42:11 -0500

    Initial set up for click evennt to add to database

--- client/src/containers/searchbar.jsx
+++ client/src/containers/searchbar.jsx
@@ -23,6 +23,7 @@ class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
 
+
     this.props.searchEvents(this.state.term || 'New York');
     this.setState({ term: '' });
     browserHistory.push('/results');
