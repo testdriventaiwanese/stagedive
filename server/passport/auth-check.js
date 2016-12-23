96ba5ccd2d38f09c83fb06dfb7b1a0346f204a72
Adam Wang <adamw3494@gmail.com>
2016-12-23 16:00:58 -0500

    fix(authcheck): fix authcheck

--- server/passport/auth-check.js
+++ server/passport/auth-check.js
@@ -5,12 +5,12 @@ const userModel = require('../user/userModel');
  *  The Auth Checker middleware function.
  */
 module.exports = (req, res, next) => {
-  if (!req.headers.authorization) {
+  if (!req.headers.authheader) {
     return res.status(401).end();
   }
 
   // get the last part from a authorization header string like "bearer token-value"
-  const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authheader;
 
   // decode the token using a secret key-phrase
   return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
