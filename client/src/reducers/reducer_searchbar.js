c4e6955ae7ea2b5db203ae341921d8696521240b
Timothy Yin <tyin92@gmail.com>
2016-12-27 17:02:01 -0500

    Added 'No Results Found' when no events are found, instead of error

--- client/src/reducers/reducer_searchbar.js
+++ client/src/reducers/reducer_searchbar.js
@@ -1,8 +1,13 @@
 export default function(state = [], action) {
   switch(action.type) {
   case 'SEARCH_EVENTS':
-  console.log('ACTION GOT: ', action.payload.data._embedded.events);
-    return action.payload.data._embedded.events;
+  console.log('ACTION GOT: ', action.payload);
+    if(!action.payload.data._embedded) {
+      return [];
+    }
+    else {
+      return action.payload.data._embedded.events;
+    }
   }
   return state;
 }
