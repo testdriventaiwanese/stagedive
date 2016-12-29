57781ed266aebeca7540beabec80a3fe3a18f82c
Timothy Yin <tyin92@gmail.com>
2016-12-29 17:36:54 -0500

    Added search artists api call

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,24 +1,26 @@
 import { combineReducers } from 'redux';
 import EventsReducer from './reducer_events';
 import ActiveEvent from './reducer_active_event';
-import SearchBar from './reducer_searchbar';
+import SearchEvents from './reducer_search_events';
 import SaveResult from './reducer_save_result';
 import GetEvents from './reducer_get_events';
 import GetUserInfo from './reducer_get_userinfo';
 import SignUp from './reducer_sign_up';
 import LogIn from './reducer_log_in';
-import RemoveEvent from './reducer_remove_event'
+import RemoveEvent from './reducer_remove_event';
+import SearchArtists from
 
 const rootReducer = combineReducers({
   events: EventsReducer,
   activeEvent: ActiveEvent,
-  searchEvents: SearchBar,
+  searchEvents: SearchEvents,
   saveResult: SaveResult,
   getEvents: GetEvents,
   getUserInfo: GetUserInfo,
   signUp: SignUp,
   logIn: LogIn,
   removeEvent: RemoveEvent,
+  searchArtists: SearchArtists,
 });
 
 export default rootReducer;
