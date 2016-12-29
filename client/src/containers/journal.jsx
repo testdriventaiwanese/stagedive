80d055a89a436c1998a6673c542ee6e07d9e91bb
Timothy Yin <tyin92@gmail.com>
2016-12-28 20:07:23 -0500

    Successfully filtering events in journal as past events

--- client/src/containers/journal.jsx
+++ client/src/containers/journal.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 // import { selectEvent } from '../actions/index';
-import { getEvents, removeEvent } from '../actions/index';
+import { getEvents, removeEvent, getUserInfo } from '../actions/index';
 import AppBar from '../containers/app-bar';
 
 class Journal extends Component {
   componentWillMount() {
     this.props.getEvents();
+    this.props.getUserInfo();
   }
 
   renderList() {
@@ -19,7 +20,15 @@ class Journal extends Component {
     let imageStyle = {
       width: '100%',
     };
-    return this.props.events.map((event) => {
+    const dateStr = this.props.userInfo[0].createdOn.slice(0,19);
+    let currentDate = new Date(dateStr);
+
+    let pastEvents = this.props.events.filter((event) => {
+      let eventDate = new Date(event.date.slice(0,19));
+      return eventDate < currentDate;
+    });
+
+    return pastEvents.map((event) => {
       let date = event.date.slice(5, 10) + '-' + event.date.slice(0, 4);
       let time = event.date.slice(11, 16);
       return (
@@ -60,12 +69,13 @@ function mapStateToProps(state) {
   // Whatever is returned will show up as props
   // inside of BookList
   return {
-    events: state.getEvents
+    events: state.getEvents,
+    userInfo: state.getUserInfo,
   };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ removeEvent, getEvents }, dispatch);
+  return bindActionCreators({ removeEvent, getEvents, getUserInfo }, dispatch);
 }
 
 
