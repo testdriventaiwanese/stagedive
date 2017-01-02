8713b986cd32b4a03445afd9da9fb4272be19d0e
Timothy Yin <tyin92@gmail.com>
2017-01-02 10:44:42 -0500

    Fixed concert journal to correctly use current date

--- client/src/reducers/reducer_get_events.js
+++ client/src/reducers/reducer_get_events.js
@@ -1,6 +1,7 @@
 export default function(state = [], action) {
   switch(action.type) {
     case 'GET_EVENTS':
+    console.log('THIS IS THE GET EVENTS REDUCER: ', action.payload);
       return action.payload.data.length === 0 ? state : action.payload.data;
     default:
       return state;
