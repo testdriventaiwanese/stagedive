e6274fdbf09999265406efb512c4bb80508f256b
Adam Wang <adamw3494@gmail.com>
2017-01-03 16:59:48 -0500

    feat(event detail): can access event detail page

--- client/src/containers/event-detail.jsx
+++ client/src/containers/event-detail.jsx
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Paper from 'material-ui/Paper'
+
 
 class EventDetail extends Component {
-  render() {
+  renderEvent() {
     if (!this.props.event) {
-      return <div></div>;
+      return <div>Event Not Listed</div>;
     }
+    const eventArr = this.props.event.events.filter((event) => {
+      return event.id === Number(this.props.params.eventId);
+    });
+    let imageDiv = {
+      width: '35%',
+      float: 'left',
+      height: '248px',
+      margin: '10px',
+    };
+    let imageStyle = {
+      width: '100%',
+    };
+    return (
+      <Paper key={eventArr[0].id} className="list-group-item" zDepth={2}>
+        <div style={imageDiv}>
+            <img src={eventArr[0].image} style={imageStyle}></img>
+        </div>
+        <div>
+          <p>{eventArr[0].name}</p>
+        </div>
+      </Paper>
+    )
+  }
 
+  render() {
     return (
       <div>
-        <h3>Details for:</h3>
-        <div>Title: {this.props.event.name}</div>
-        <div>ID: {this.props.event.id}</div>
+        <h3>Event Details</h3>
+        <div className="list-group col-sm-16">
+          {this.renderEvent()}
+        </div>
       </div>
     );
   }
@@ -19,8 +47,9 @@ class EventDetail extends Component {
 
 function mapStateToProps(state) {
   return {
-    event: state.activeEvent
+    event: state.getEvents,
   };
 }
+
 
 export default connect(mapStateToProps)(EventDetail);
