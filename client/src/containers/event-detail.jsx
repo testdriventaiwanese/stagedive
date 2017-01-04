4f9ec127100364da2ecf79012305968c7a1b433e
Adam Wang <adamw3494@gmail.com>
2017-01-03 20:13:30 -0500

    style(event detail): more info being shown in event detail

--- client/src/containers/event-detail.jsx
+++ client/src/containers/event-detail.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import Paper from 'material-ui/Paper'
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import { removeEvent } from '../actions/index';
 
 
 class EventDetail extends Component {
@@ -12,9 +14,12 @@ class EventDetail extends Component {
     const eventArr = this.props.event.events.filter((event) => {
       return event.id === Number(this.props.params.eventId);
     });
+    const event = eventArr[0];
+    const saleTimes = JSON.parse(event.sale_date);
+    const artistInfo = JSON.parse(event.artist_name);
     let imageDiv = {
-      width: '35%',
-      float: 'left',
+      width: '45%',
+      float: 'center',
       height: '248px',
       margin: '10px',
     };
@@ -22,15 +27,29 @@ class EventDetail extends Component {
       width: '100%',
     };
     return (
-      <Paper key={eventArr[0].id} className="list-group-item" zDepth={2}>
+      <Paper key={event.id} className="list-group-item" zDepth={2}>
         <div style={imageDiv}>
-            <img src={eventArr[0].image} style={imageStyle}></img>
+            <img src={event.image} style={imageStyle}></img>
         </div>
         <div>
-          <p>{eventArr[0].name}</p>
+          <h2>{event.name}</h2>
+          <p>{artistInfo.name}</p>
+          <p>{event.city}</p>
+          <p>{event.country}</p>
+          <p>Event Date: {event.date}</p>
+          <div>
+            <h6>Tickets</h6>
+            <p>On sale date: {saleTimes.startDateTime}</p>
+            <p><a href={event.event_url}>Buy Tickets</a></p>
+          </div>
         </div>
+        <RaisedButton
+          label="Remove Event"
+          secondary
+          onClick={() => this.props.removeEvent(event.tm_id)}
+        />
       </Paper>
-    )
+    );
   }
 
   render() {
@@ -51,5 +70,9 @@ function mapStateToProps(state) {
   };
 }
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ removeEvent }, dispatch);
+}
 
-export default connect(mapStateToProps)(EventDetail);
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventDetail);
