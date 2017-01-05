e2984416ea3bbff657a6e04803221405a6b82058
Timothy Yin <tyin92@gmail.com>
2017-01-05 17:11:49 -0500

    Journal photos rendering and disappearing immediately

--- client/src/containers/journal-photo.jsx
+++ client/src/containers/journal-photo.jsx
@@ -1,5 +1,6 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Paper from 'material-ui/Paper';
 
 class JournalPhoto extends Component {
   render() {
@@ -14,24 +15,20 @@ class JournalPhoto extends Component {
       width: '100%',
     };
 // /${event.tm_id}
-  console.log('EVENTS OBJECT IN JOURNAL: ', this.props.events);
-    return this.props.events.pastEvents.map((event) => {
+  console.log('event IN JOURNAL PHOTO: ', event);
       let date = event.date.slice(5, 10) + '-' + event.date.slice(0, 4);
       let time = event.date.slice(11, 16);
       let userId = this.props.userInfo
       return (
-        <Paper style={imageDiv} zDepth={2}>
+      <Paper style={imageDiv} zDepth={2}>
         <div key={event.id}>
           <Link to={`/journal/${id}/${event.id}`}>
-            <img src={event.image} style={imageStyle}/>
+            <img src={event.image} style={imageStyle} />
           </Link>
-          <span><strong>{event.name}</strong></span>
-          <p>Date: {date}</p>
         </div>
       </Paper>
       );
-    });
-  }
+}
 }
 
 export default JournalPhoto;
