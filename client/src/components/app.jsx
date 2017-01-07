4e625c3f8a6aeed0895caa039671013a6dc21189
Adam Wang <adamw3494@gmail.com>
2017-01-06 20:48:32 -0500

    feat(front end auth): local login works, facebook incomplete

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { hashHistory } from 'react-router';
 import AppBar from '../containers/app-bar';
+import Auth from '../modules/auth';
+
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 export default class App extends Component {
+  componentWillMount() {
+    if (!Auth.isUserAuthenticated()) {
+      hashHistory.push('/login');
+    }
+  }
+
   render() {
     return (
       <MuiThemeProvider>
--- client/src/containers/token.jsx
+++ client/src/containers/token.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { hashHistory } from 'react-router';
 
 class Token extends Component {
+  componentWillMount() {
+    // const token = window.location.hash.slice(8, 148);
+    // const userId = window.location.hash.slice(148);
+    console.log('window location: ', window.location);
+    localStorage.setItem('token', window.location.hash.slice(8, 148));
+    localStorage.setItem('id', window.location.hash.slice(148));
+    // hashHistory.push('/');
+  }
   componentDidMount() {
-    const token = window.location.hash.slice(8, 148);
-    const userId = window.location.hash.slice(148);
-    console.log('window userId: ', userId);
-    localStorage.setItem('token', token);
-    localStorage.setItem('id', userId);
+    // const token = window.location.hash.slice(8, 148);
+    // const userId = window.location.hash.slice(148);
+    console.log('window location: ', window.location);
+    localStorage.setItem('token', window.location.hash.slice(8, 148));
+    localStorage.setItem('id', window.location.hash.slice(148));
     hashHistory.push('/');
   }
 
