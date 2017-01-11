7e898e117ee456c318e31f4820caed5b995b0b1e
Timothy Yin <tyin92@gmail.com>
2017-01-11 16:14:07 -0500

    Added tabs on home page

--- client/src/containers/event-list.jsx
+++ client/src/containers/event-list.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import RaisedButton from 'material-ui/RaisedButton';
 import CircularProgress from 'material-ui/CircularProgress';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Tabs, Tab} from 'material-ui/Tabs';
 import IconMenu from 'material-ui/IconMenu';
 import FlatButton from 'material-ui/FlatButton';
 import MenuItem from 'material-ui/MenuItem';
@@ -13,7 +14,8 @@ import Snackbar from 'material-ui/Snackbar';
 import LinearProgress from 'material-ui/LinearProgress';
 import { Link, hashHistory } from 'react-router';
 import moment from 'moment';
-import { getUserEvents, getUserInfo, removeEvent, getEvents } from '../actions/index';
+import FriendsEvents from './friends-events';
+import { getUserEvents, getUserInfo, removeEvent, getEvents, getFriendsEvents } from '../actions/index';
 import Auth from '../modules/auth';
 
 class EventList extends Component {
@@ -21,6 +23,7 @@ class EventList extends Component {
     super(props);
     this.state = {
       open: false,
+      value: 'myEvents',
     };
   }
   componentWillMount() {
@@ -28,6 +31,7 @@ class EventList extends Component {
     const user = { id };
     this.props.getUserInfo();
     this.props.getUserEvents(user);
+    this.props.getFriendsEvents();
   }
   handleTouchTap() {
     this.setState({
@@ -39,34 +43,42 @@ class EventList extends Component {
       open: false,
     });
   }
+  handleChangeTab(value) {
+    this.setState({
+      value,
+    });
+  }
   renderList() {
     const id = localStorage.getItem('id');
+    const cardStyle = {
+      marginBottom: '10px',
+    };
     const imageStyle = {
       width: '100%',
     };
     const menuStyle = {
       float: 'right',
       height: '0%',
-    }
+    };
 
     const largestPic = (imageArray) => {
       let largest = 0;
       let index;
       imageArray.forEach((image, i) => {
-        if(image.width > largest) {
+        if (image.width > largest) {
           largest = image.width;
           index = i;
         }
       });
       return imageArray[index].url;
-    }
+    };
 
-    if(!this.props.events.futureEvents) {
+    if (!this.props.events.futureEvents) {
       return (
         <div>
           <LinearProgress mode="indeterminate" />
         </div>
-      )
+      );
     }
 
     return this.props.events.futureEvents.slice(1).map((event, i) => {
@@ -76,9 +88,9 @@ class EventList extends Component {
       const artist = JSON.parse(event.artist_name).map((performer) => performer.name);
       const date = momentDate.toString() + ' ' + est.toString().slice(34);
       let image = null;
-      if(event.image){
+      if (event.image) {
         image = largestPic(JSON.parse(event.image)) || null;
-      };
+      }
       const venue = JSON.parse(event.venue)[0];
       let venueName = null;
       let venueStateOrCountry = null;
@@ -90,60 +102,67 @@ class EventList extends Component {
         venueStateOrCountry = venue.country.countryCode;
       }
       return (
-        <Card key={event.id} className="list-group-item" zDepth={1}>
+        <Card key={event.id} className="list-group-item" style={cardStyle} zDepth={1}>
           <CardMedia
-            overlay={ <CardTitle
-              title={event.name}
-              subtitle={venue.city.name + ', ' + momentFromNow.toString()}
-              />}
-              >
-              <img src={image} style={imageStyle} />
-            </CardMedia>
-            <IconMenu
-              style={menuStyle}
-              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-              anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-              targetOrigin={{horizontal: 'right', vertical: 'top'}}
-              >
-              <Link to={`/event/${id}/${event.id}`}>
-                <MenuItem primaryText="View Event Details" secondary/>
-              </Link>
-              <MenuItem
-                primaryText="Remove Event"
-                secondary
-                onTouchTap={this.handleTouchTap}
-                onClick={() => this.props.removeEvent(event.tm_id, i+1)}
-                />
-              <Snackbar
-                open={this.state.open}
-                message="Event Removed"
-                autoHideDuration={4000}
-                onRequestClose={this.handleRequestClose}
-                />
-            </IconMenu>
-            <CardText>
-              <span><strong>Listed acts: </strong>{artist.join(', ')}</span>
-              <br />
-              <span><strong>Venue: </strong>{venue.name}</span>
-              <br />
-              <span><strong>Event Start: </strong>{date}</span>
-            </CardText>
-            <CardActions>
-              <Link to={`/event/${id}/${event.id}`}>
-                <FlatButton label='Event Details' secondary />
-              </Link>
-              <a href={event.event_url}><FlatButton label='Buy Tickets' secondary /></a>
-            </CardActions>
-          </Card>
+            overlay={
+              <CardTitle
+                title={event.name}
+                subtitle={venue.city.name + ', ' + momentFromNow.toString()}
+              />
+            }
+          >
+            <img src={image} style={imageStyle} />
+          </CardMedia>
+          <IconMenu
+            style={menuStyle}
+            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            >
+            <Link to={`/event/${id}/${event.id}`}>
+              <MenuItem primaryText="View Event Details" secondary/>
+            </Link>
+            <MenuItem
+              primaryText="Remove Event"
+              secondary
+              onTouchTap={this.handleTouchTap}
+              onClick={() => this.props.removeEvent(event.tm_id, i+1)}
+              />
+            <Snackbar
+              open={this.state.open}
+              message="Event Removed"
+              autoHideDuration={4000}
+              onRequestClose={this.handleRequestClose}
+              />
+          </IconMenu>
+          <CardText>
+            <span><strong>Listed acts: </strong>{artist.join(', ')}</span>
+            <br />
+            <span><strong>Venue: </strong>{venue.name}</span>
+            <br />
+            <span><strong>Event Start: </strong>{date}</span>
+          </CardText>
+          <CardActions>
+            <Link to={`/event/${id}/${event.id}`}>
+              <FlatButton label='Event Details' secondary />
+            </Link>
+            <a href={event.event_url}><FlatButton label='Buy Tickets' secondary /></a>
+          </CardActions>
+        </Card>
       );
     });
   }
   render() {
     return (
-        <div>
+      <Tabs>
+        <Tab label="My Events" value="myEvents" >
           {this.renderList()}
-        </div>
-    )
+        </Tab>
+        <Tab label="Friends Events" value="friendsEvents">
+          <FriendsEvents { ...this.props }/>
+        </Tab>
+      </Tabs>
+    );
   }
 }
 
@@ -154,7 +173,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ getUserEvents, getUserInfo, removeEvent, getEvents }, dispatch);
+  return bindActionCreators({ getUserEvents, getUserInfo, removeEvent, getEvents, getFriendsEvents }, dispatch);
 }
 
 
--- client/src/containers/friends-events.jsx
+++ client/src/containers/friends-events.jsx
@@ -11,9 +11,9 @@ import LinearProgress from 'material-ui/LinearProgress';
 import { getFriendsEvents } from '../actions/index';
 
 class FriendsEventList extends Component {
-  componentWillMount() {
-    this.props.getFriendsEvents();
-  }
+  // componentWillMount() {
+  //   this.props.getFriendsEvents();
+  // }
   renderList() {
     const imageDiv = {
       width: '35%',
@@ -50,7 +50,7 @@ class FriendsEventList extends Component {
             <span>No upcoming events</span>
             <br />
           </CardText>
-        </Card>  
+        </Card>
       )
     }
     const userInfo = this.props.events.userInfo;
@@ -115,15 +115,15 @@ class FriendsEventList extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    events: state.getFriendsEvents,
-  };
-}
+// function mapStateToProps(state) {
+//   return {
+//     events: state.getFriendsEvents,
+//   };
+// }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ getFriendsEvents }, dispatch);
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(FriendsEventList);
+export default connect(null, mapDispatchToProps)(FriendsEventList);
