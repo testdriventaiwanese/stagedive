ba6b36c080f1d208c9b8bdd372a60839c731a602
Timothy Yin <tyin92@gmail.com>
2017-01-12 21:03:01 -0500

    refactor event model and controller to knex

--- server/event/eventController.js
+++ server/event/eventController.js
@@ -5,47 +5,60 @@ module.exports = {
   events: {
     getUserEvents(req, res) {
       const id = jwt.decode(req.headers.authheader, process.env.JWT_SECRET);
-      eventModel.events.getUserEvents(id.sub, (results) => {
-        if (!results) {
-          console.log('ERROR in getting all');
-          res.sendStatus(401);
-        } else {
-          res.status(200).send(results);
-        }
-      });
+      eventModel.events.getUserEvents(id.sub)
+      .then((results) => {
+        res.status(200).send(results)
+      })
+      // , (results) => {
+      //   if (!results) {
+      //     console.log('ERROR in getting all');
+      //     res.sendStatus(401);
+      //   } else {
+      //     res.status(200).send(results);
+      //   }
+      // });
     },
     getOtherUserEvents(req, res) {
       const otherUserId = req.headers.userid;
-      eventModel.events.getUserEvents(otherUserId, (results) => {
-        if (!results) {
-          console.log('ERROR in getting all');
-          res.sendStatus(401);
-        } else {
-          res.status(200).send(results);
-        }
-      });
+      eventModel.events.getUserEvents(otherUserId)
+      .then((results) => {
+        res.status(200).send(results);
+      })
+      // , (results) => {
+      //   if (!results) {
+      //     console.log('ERROR in getting all');
+      //     res.sendStatus(401);
+      //   } else {
+      //   }
+      // });
     },
     getFriendsEvents(req, res) {
       const id = jwt.decode(req.headers.authheader, process.env.JWT_SECRET);
-      eventModel.events.getFriendsEvents(id.sub, (results) => {
-        if (!results) {
-          console.log('ERROR in getting all');
-          res.sendStatus(401);
-        } else {
-          res.status(200).send(results);
-        }
-      });
+      eventModel.events.getFriendsEvents(id.sub)
+      .then((results) => {
+        res.status(200).send(results);
+      })
+      // , (results) => {
+      //   if (!results) {
+      //     console.log('ERROR in getting all');
+      //     res.sendStatus(401);
+      //   } else {
+      //   }
+      // });
     },
     search({ body: { name } }, res) {
-      eventModel.events.searchEvents(name, (results) => {
-        if (!results) {
-          console.log('ERROR in searching');
-          res.sendStatus(401);
-        } else {
-          console.log('searched');
-          res.sendStatus(200);
-        }
-      });
+      eventModel.events.searchEvents(name)
+      .then((results) => {
+        res.sendStatus(200);
+      })
+      // , (results) => {
+      //   if (!results) {
+      //     console.log('ERROR in searching');
+      //     res.sendStatus(401);
+      //   } else {
+      //     console.log('searched');
+      //   }
+      // });
     },
     addEvent({ body: {
       tm_id,
@@ -74,27 +87,35 @@ module.exports = {
         longitude,
         sale_date,
       ];
-      eventModel.events.addEvent(userId.sub, params, (results) => {
-        if (!results) {
-          console.log('Issue in adding EVENT to database');
-          res.sendStatus(401);
-        } else {
-          console.log('RESULTS FROM ADD EVENT: ', results);
-          res.status(200).send(results);
-        }
-      });
+      eventModel.events.addEvent(userId.sub, params)
+      .then((results) => {
+        res.status(200).send(results);
+
+      })
+      // , (results) => {
+      //   if (!results) {
+      //     console.log('Issue in adding EVENT to database');
+      //     res.sendStatus(401);
+      //   } else {
+      //     console.log('RESULTS FROM ADD EVENT: ', results);
+      //   }
+      // });
     },
 
     showUserEvents({ body: { userId } }, res) {
-      eventModel.events.userEvents(userId, (results) => {
-        if (!results) {
-          console.log('ERROR');
-          res.sendStatus(401);
-        } else {
-          console.log(res);
-          res.sendStatus(200);
-        }
-      });
+      eventModel.events.userEvents(userId)
+      .then((results) => {
+        res.sendStatus(200);
+
+      })
+      // , (results) => {
+      //   if (!results) {
+      //     console.log('ERROR');
+      //     res.sendStatus(401);
+      //   } else {
+      //     console.log(res);
+      //   }
+      // });
     },
 
     showLocalEvents({ body: { location, city, zipcode } }, res) {
@@ -111,29 +132,36 @@ module.exports = {
     },
 
     showRelatedEvents({ body: { genre } }, res) {
-      eventModel.events.relatedEvents(genre, (results) => {
-        if (!results) {
-          console.log('Issue in showing related events');
-          res.sendStatus(401);
-        } else {
-          console.log(res);
-          res.sendStatus(200);
-        }
-      });
+      eventModel.events.relatedEvents(genre)
+      .then((results) => {
+        res.sendStatus(200);
+
+      })
+      // , (results) => {
+      //   if (!results) {
+      //     console.log('Issue in showing related events');
+      //     res.sendStatus(401);
+      //   } else {
+      //     console.log(res);
+      //   }
+      // });
     },
 
     deleteEvent(req, res) {
       const id = jwt.decode(req.headers.authheader, process.env.JWT_SECRET);
       const params = [req.body.tm_id, id.sub];
-      eventModel.events.deleteEvent(params, (results) => {
-        if (!results) {
-          console.log('Issue in removing events');
-          res.sendStatus(401);
-        } else {
-          console.log('removed');
-          res.sendStatus(200);
-        }
-      });
+      eventModel.events.deleteEvent(params)
+      .then((results) => {
+        res.sendStatus(200);
+      })
+      // , (results) => {
+      //   if (!results) {
+      //     console.log('Issue in removing events');
+      //     res.sendStatus(401);
+      //   } else {
+      //     console.log('removed');
+      //   }
+      // });
     },
   },
 };
