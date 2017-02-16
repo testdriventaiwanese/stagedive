436b39e8cd398ca8d9f2ddcb6232e19d4be302b5
Timothy Yin <tyin92@gmail.com>
2017-02-16 15:26:44 -0500

    Clean up styling of journal code

--- client/src/containers/journal-photo.jsx
+++ client/src/containers/journal-photo.jsx
@@ -10,10 +10,6 @@ class JournalPhoto extends Component {
   render() {
     const { event, i, comments } = this.props;
     const id = this.props.params.userId;
-    const imageDiv = {
-      width: '100%',
-      height: '250px',
-    };
     const imageStyle = {
       height: '250px',
       textAlign: 'center',
